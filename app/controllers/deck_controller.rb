426d9b616b92b948fcfcef0da0f60f9fe1c3a218
Aaron Tsai <email": null>
2015-12-05 19:11:50 -0500

    Can only play once, no stats implemented

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -16,15 +16,20 @@ get '/deck/:deck_id' do
   @deck = Deck.find_by(id: params[:deck_id])
   # p @temp_deck.slice!(params[:card_id])
   # p params
+  @round = Round.find_by(user_id: session[:session_id])
+  @cards.each do |card|
+      Guess.find_or_create_by(card_id: card.id, round_id: session[:session_id])
+  end
   erb :'/deck/show'
 end
 
 post '/deck/:deck_id' do
   @cards = Card.where(deck_id: params[:deck_id])
   @deck = Deck.find_by(id: params[:deck_id])
-  @temp_deck = @deck.dup
-  p @temp_deck
-  erb :'/deck/show'
+  # @temp_deck = @cards.dup
+  @round = Round.find_by(user_id: session[:session_id])
+
+  redirect :"/deck/#{params[:deck_id]}/play"
 end
 
 get '/deck/:deck_id/card/:card_id' do
@@ -34,13 +39,33 @@ get '/deck/:deck_id/card/:card_id' do
 end
 
 post '/deck/:deck_id/card/:card_id' do
-  @round = Round.new(user_id: session[:user_id], deck_id: params[:deck_id])
+  @round = Round.new(user_id: session[:session_id], deck_id: params[:deck_id])
   @cards = Card.where(deck_id: params[:deck_id])
+  @guess = Guess.find_by(round_id: session[:session_id], card_id: params[:card_id])
+
   if @round.save
     redirect "/deck/#{params[:deck_id]}/card/#{params[:card_id]}"
   else
-    redirect "/deck/#{params[:deck_id]}/card/#{params[:card_id]}"
+    redirect "/deck/#{params[:deck_id]}"
   end
 end
 
+get '/deck/:deck_id/play' do
+  @deck = Deck.find_by(id: params[:deck_id])
+  @cards = Card.where(deck_id: params[:deck_id])
+  # binding.pry
+  @guess = Guess.where(round_id: session[:session_id], correct?: false)
+  # binding.pry
+  erb :'deck/play'
+end
 
+
+post '/deck/:deck_id/play' do
+  @guess = Guess.find_by(round_id: session[:session_id], card_id: params[:card_id])
+  # binding.pry
+  if params[:correct] == "Correct"
+    @guess[:correct?] = true
+    @guess.save
+  end
+  redirect "/deck/#{params[:deck_id]}/play"
+end
